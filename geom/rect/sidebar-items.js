c4a36d227938fc52cdb7128736b930226e7dab29
Brandon King <wektorious@gmail.com>
2015-05-26 08:06:57 +0000

    rebuild pages at 3e407d9

--- geom/rect/sidebar-items.js
+++ geom/rect/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Iter",""],["Rect",""]]});
+initSidebarItems({"struct":[["Iter","An iterator over a rectangle, returning each point within the rectangle going from left-to-right and top-to-bottom."],["Rect","A generic rectangle structure."]]});
