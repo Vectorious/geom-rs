4533ae53cf76d6533a03153628b5a29439b465a5
Brandon King <yoman650@gmail.com>
2015-05-25 21:02:21 -0700

    Change how One trait is implemented

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,14 @@ pub mod rect;
 pub type Point<T> = point::Point<T>;
 pub type Rect<T> = rect::Rect<T>;
 
-trait One<T>
-    where T: Add<i32, Output=T> +
-             Default
+trait One<T> {
+    fn one() -> T;
+}
+
+impl<T> One<T> for T
+    where T: Add<i32, Output=T> + Default
 {
     fn one() -> T {
         T::default() + 1
     }
 }
-
-impl<T> One<T> for T
-    where T: Default + Add<i32, Output=T>
-{ /* derived implementor */ }
