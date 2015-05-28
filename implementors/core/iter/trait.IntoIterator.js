cf4e2a3589f491c43d4f507787e5b5a4d1f36878
Brandon King <wektorious@gmail.com>
2015-05-28 01:27:44 +0000

    rebuild pages at 1584b35

--- implementors/core/iter/trait.IntoIterator.js
+++ implementors/core/iter/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['geom'] = ["impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for <a class='struct' href='geom/rect/struct.Rect.html' title='geom::rect::Rect'>Rect</a>&lt;T&gt;",];
+implementors['geom'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for <a class='struct' href='geom/rect/struct.Rect.html' title='geom::rect::Rect'>Rect</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
